e19ea237abc136f91d1209d155040a45ef72671b
Rob <robbiestells@hotmail.com>
2016-10-28 16:55:22 -0400

    Initial commit

---
+++ app/src/main/java/data/PetDbHelper.java
@@ -0,0 +1,37 @@
+package data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import static android.os.FileObserver.CREATE;
+
+/**
+ * Created by Rob on 10/28/2016.
+ */
+
+public class PetDbHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "shelter.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public PetDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String SQL_CREATE_PETS_TABLE = "CREATE TABLE " + PetContract.PetEntry.TABLE_NAME + " ("
+                + PetContract.PetEntry.I_D + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + PetContract.PetEntry.COLUMN_PET_NAME + " TEXT NOT NULL, "
+                + PetContract.PetEntry.COLUMN_PET_BREED + " TEXT, "
+                + PetContract.PetEntry.COLUMN_PET_GENDER + " INTEGER NOT NULL, "
+                + PetContract.PetEntry.COLUMN_PET_WEIGHT + " INTEGER NOT NULL DEFAULT 0);";
+
+        db.execSQL(SQL_CREATE_PETS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
