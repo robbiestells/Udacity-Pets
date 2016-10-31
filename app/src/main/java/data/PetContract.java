5c267c946e0afb648a2ef48004febf398757ce1c
Rob <robbiestells@hotmail.com>
2016-10-31 19:40:27 -0400

    Middle of Lesson 3

--- app/src/main/java/data/PetContract.java
+++ app/src/main/java/data/PetContract.java
@@ -1,5 +1,6 @@
 package data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -9,7 +10,15 @@ import android.provider.BaseColumns;
 public final class PetContract {
     private PetContract(){}
 
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_PETS = "pets";
+
     public static class PetEntry implements BaseColumns {
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
+
         public static final String TABLE_NAME="pets";
 
         public static final String I_D=BaseColumns._ID;
---
+++ app/src/main/java/data/PetProvider.java
@@ -0,0 +1,64 @@
+package data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Rob on 10/31/2016.
+ */
+
+public class PetProvider extends ContentProvider{
+
+    public static final String LOG_TAG = PetProvider.class.getSimpleName();
+
+    private PetDbHelper mHelper;
+
+    private static final int PETS = 100;
+
+    private static final int PETS_ID = 101;
+
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static {
+        sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS, PETS);
+        sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS + "/#", PETS_ID);
+    }
+
+    @Override
+    public boolean onCreate() {
+        mHelper = new PetDbHelper(getContext());
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return 0;
+    }
+}
