38a416bcbff0de5225e3e535cd1df90589c03343
robbiestells <robbiestells@hotmail.com>
2016-11-01 12:42:18 -0400

    Update PetProvider.java

--- app/src/main/java/data/PetProvider.java
+++ app/src/main/java/data/PetProvider.java
@@ -6,6 +6,7 @@ import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 /**
  * Created by Rob on 10/31/2016.
@@ -65,8 +66,27 @@ public class PetProvider extends ContentProvider{
 
     @Nullable
     @Override
-    public Uri insert(Uri uri, ContentValues values) {
-        return null;
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PETS:
+                return insertPet(uri,contentValues);
+            default:
+                throw new IllegalArgumentException("Insertion is not supported for " + uri);
+        }
+    }
+
+    private Uri insertPet(Uri uri, ContentValues contentValues){
+        SQLiteDatabase database = mHelper.getWritableDatabase();
+
+        long id = database.insert(PetContract.PetEntry.TABLE_NAME, null, contentValues);
+
+        if (id == -1){
+            Log.e(LOG_TAG, "Failed to insert row for " + uri);
+            return null;
+        }
+
+        return  ContentUris.withAppendedId(uri, id);
     }
 
     @Override
